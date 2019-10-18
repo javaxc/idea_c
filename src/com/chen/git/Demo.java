f4649d9b1f6e87baa85b54fb61865f5c33702edc
javaxc <2997791530@qq.com>
2019-10-18 14:36:14 +0800

    git res

--- src/com/chen/git/Demo.java
+++ src/com/chen/git/Demo.java
@@ -6,6 +6,7 @@ package com.chen.git;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("git ddd test");
+        System.out.println("git ddd test");
 
     }
 }
